b0d48224cfabd3f4fe29a11c40c79fc816dc0692
DomicaCode <dokiiscool35@gmail.com>
2018-09-12 09:38:29 +0200

    first commit

---
+++ app/etc/env.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_domica'
+    ],
+    'crypt' => [
+        'key' => '2dfa241e9159068728ca738d4f993cb6'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => '',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'translate' => 1,
+        'config_webservice' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 11 Sep 2018 17:33:20 +0000'
+    ]
+];
